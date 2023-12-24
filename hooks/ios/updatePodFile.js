f105277eb11c7513635ee446350289d61b58f96a
Vedant Sasane <vedant.sasane@wolkus.com>
2023-12-24 19:13:33 +0530

    Script For Updating PodFile

--- hooks/ios/updatePodFile.js
+++ hooks/ios/updatePodFile.js
@@ -10,17 +10,14 @@ module.exports = function (context) {
   const podfileContent = fs.readFileSync(podfilePath, "utf8");
 
   // Check if the dependency is already added
-  if (!podfileContent.includes("pod 'Firebase/Messaging'")) {
-    // Add the dependency to the Podfile
-    const updatedPodfile = podfileContent.replace(
-      /target 'NotificationServiceExtension' do[\s\S]*?end/,
-      "$&\n  pod 'Firebase/Messaging'"
-    );
 
-    // Write the updated Podfile back to the file system
-    fs.writeFileSync(podfilePath, updatedPodfile, "utf8");
-    console.log("Firebase/Messaging dependency added to NotificationServiceExtension target.");
-  } else {
-    console.log("Firebase/Messaging dependency is already present in Podfile.");
-  }
+  // Add the dependency to the Podfile
+  const updatedPodfile = podfileContent.replace(
+    /target 'NotificationServiceExtension' do[\s\S]*?end/,
+    "$&\n  pod 'Firebase/Messaging'"
+  );
+
+  // Write the updated Podfile back to the file system
+  fs.writeFileSync(podfilePath, updatedPodfile, "utf8");
+  console.log("Firebase/Messaging dependency added to NotificationServiceExtension target.");
 };
